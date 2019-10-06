1c38e9686965368a117aaf04afaa496e18839b44
originbenntou8973 <originbenntou8973@gmail.com>
2019-10-07 02:44:31 +0900

    ログ出し

--- front/main.go
+++ front/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"github.com/gorilla/mux"
 	"github.com/originbenntou/E-Kitchen/front/handler"
 	"github.com/originbenntou/E-Kitchen/front/middleware"
+	"google.golang.org/grpc"
 	"log"
 	"net/http"
+	"time"
 
-	_ "github.com/originbenntou/E-Kitchen/proto/user"
+	pbUser "github.com/originbenntou/E-Kitchen/proto/user"
 )
 
 const port = ":8080"
@@ -18,23 +21,23 @@ func main() {
 	r.Use(middleware.Logging)
 
 	// TODO:関数に切り出す
-	//target := "e-kitchen-user:50051"
-	//conn, err := grpc.Dial(target, grpc.WithInsecure())
-	//if err != nil {
-	//	log.Fatalf("did not connect: %s", err)
-	//}
-	//defer conn.Close()
-	//client := pbUser.NewUserServiceClient(conn)
-	//
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	//defer cancel()
-	//
-	//resp, err := client.CreateUser(ctx, &pbUser.CreateUserRequest{
-	//	Email: "hogehoge",
-	//	Password: []byte{},
-	//})
-	//
-	//log.Println(resp)
+	target := "e-kitchen-user:50051"
+	conn, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+	client := pbUser.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	resp, err := client.CreateUser(ctx, &pbUser.CreateUserRequest{
+		Email:    "hogehoge",
+		Password: []byte{},
+	})
+
+	log.Println(resp)
 
 	r.Path("/").Methods(http.MethodGet).HandlerFunc(handler.LoginHandler)
 	r.Path("/signup").Methods(http.MethodGet).HandlerFunc(handler.SignupHandler)
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/originbenntou/E-Kitchen
 
 require (
+	github.com/golang/protobuf v1.3.2
 	github.com/gorilla/mux v1.7.3 // indirect
-	google.golang.org/grpc v1.24.0 // indirect
+	google.golang.org/grpc v1.24.0
 )
